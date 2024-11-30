ce7c4ccf808b6ae40762232094c3a87ca7146248
Kanit5 <kanitmujka@gmail.com>
2024-11-30 17:36:49 +0100

    kanit update

---
+++ script.js
@@ -0,0 +1,54 @@
+function validateLogIn(){
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;   
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        alert("Invalid email address");
+        return false;
+    }
+    if(pass==""){
+        alert("Fill the password please!");
+        return false;
+    }
+    if(pass.length<8){
+        alert("Password must be minimum 8 characters long");
+        return false;
+    }
+    if(pass.length>16){
+        alert("Password must be maximum 16 characters long");
+        return false;
+    }
+    return true;
+}
+
+function validateRegister(){
+    var name = document.getElementById("name").value;
+    var email2 = document.getElementById("email2").value;
+    var pass2 = document.getElementById("pass2").value;
+    var confirm = document.getElementById("passconfirm").value;
+
+    if(name == " "){
+        alert("Enter your name!");
+        return false;
+    }
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email2)){
+        alert("Invalid email address");
+        return false;
+    }
+    if(pass2==""){
+        alert("Fill the password please!");
+        return false;
+    }
+    if(pass2.length<8){
+        alert("Password must be minimum 8 characters long");
+        return false;
+    }
+    if(pass2.length>16){
+        alert("Password must be maximum 16 characters long");
+        return false;
+    }
+    if(confirm != pass2){
+        alert("Confirm your password correctly!");
+        return false;
+    }
+    return true;
+}
