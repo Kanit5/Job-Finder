d251e067299d2d856e87b980292e2438fe4d8c74
Harisi20 <130598306+Harisi20@users.noreply.github.com>
2025-01-15 21:11:16 +0100

    harisi update

--- adminlogin.php
+++ adminlogin.php
@@ -22,3 +22,52 @@
                 <a href = "post.html" class = "btn" style = "margin-top: 0;">Post Jobs</a>
         </section>
     </header>
+    <div class = "account-form-container">
+        <section class = "account-form">
+            <form action = "" method = "post" onsubmit="return validateLogIn()">
+                <h3>Welcome back admin!</h3>
+                <input type = "email" required  id = "email" name = "email" maxlength = "50" placeholder="Enter your email" class = "input">
+                <input type = "password" required id = "pass" name = "pass" maxlength="20" placeholder="Enter your password" class = "input">
+                
+                <input type = "submit" value = "Log in" name = "submit" class = "btn">
+            </form>
+        </section>
+    </div>
+    
+    <footer class="footer">
+
+        <section class="grid">
+        
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.html"><i class="fas fa-angle-right"></i>Home</a>
+            <a href="about.html"><i class="fas fa-angle-right"></i>About</a>
+            <a href="jobs.html"><i class="fas fa-angle-right"></i>All Jobs</a>
+            <a href="contact.html"><i class="fas fa-angle-right"></i>Contact Us</a>      
+        </div>
+        
+        <div class="box">
+            <h3>Extra Links</h3>
+            <a href="account.html"><i class="fas fa-angle-right"></i>Account</a>
+            <a href="register.html"><i class="fas fa-angle-right"></i>Register</a>
+            <a href="post.html"><i class="fas fa-angle-right"></i>Post Job</a>
+        
+        </div>
+        
+        
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i>Facebook</a>
+            <a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i>Instagram</a>
+            <a href="https://www.linkedin.com/"><i class="fa-brands fa-linkedin"></i>Linkedin</a>
+        </div>
+        
+        </section>
+        
+    </footer>
+        
+
+
+    <script src="script.js"></script>
+</body>
+</html>
