c93b0ad6701a8f97ce632c5bfcec2188622e85c2
Joni63 <jonihamza11@gmail.com>
2025-01-16 20:21:23 +0100

    joni update

---
+++ adminpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+    <head>
+        <meta charset = 'UTF-8'>
+        <meta http-equiv = "X-UA-Compatible" content = "IE-edge">
+        <meta name = "viewport" content = "width = device-width, initial scale = 1.0">
+        <title>Account</title>
+        <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+    <header class = "header">
+        <section class = "flex">
+            <div id = "menu-btn" class = "fas fa-bars-staggered"></div>
+            <a href = "home.html" class = "logo"><i class = "fas fa-briefcase"></i>JobFinder</a>
+            <nav class = "navbar">
+                <a href = "home.html">Home</a>
+                <a href = "about.html">About Us</a>
+                <a href = "jobs.html">All Jobs</a>
+                <a href = "contact.html">Contact Us</a>
+                <a href = "login.html">Account</a>
+</nav>
+<a href = "post.html" class = "btn" style = "margin-top: 0;">Post Jobs</a>
+</section>
+</header>
+
+
+
+
