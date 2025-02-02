fc3f984a82280221a632bd43ebe87ac587dcb83a
Kanit5 <kanitmujka@gmail.com>
2025-02-02 20:29:42 +0100

    update

--- adminpage.php
+++ adminpage.php
@@ -11,11 +11,6 @@
            $message = new Message($connection);
            $job = new Job($connection);
 
-           if(!$admin->login($email,$password)){
-            header("Location: login.php"); 
-            exit();
-        }
-
            if (isset($_GET['delete_id'])) {
             $delete_id = intval($_GET['delete_id']);
                 if ($user->deleteUsers($delete_id)) {
